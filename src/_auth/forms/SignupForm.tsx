e6ebb739b0a0c728cf9d52ec66531818ee3d1c94
Mitzi17 <m_estrada17@live.com>
2023-12-18 22:40:40 -0800

    import createUserAccount from appwrite and toast to show message if sign up is unsuccessful

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,10 +1,11 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Link } from "react-router-dom";
 
+import { useToast } from "@/components/ui/use-toast";
+
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -17,8 +18,10 @@ import { useForm } from "react-hook-form";
 import { SignupValidation } from "@/lib/validation";
 import { z } from "zod";
 import Loader from "@/components/shared/Loader";
+import { createUserAccount } from "@/lib/appwrite/api";
 
 const SignupForm = () => {
+  const { toast } = useToast();
   const isLoading = false;
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignupValidation>>({
@@ -33,7 +36,10 @@ const SignupForm = () => {
 
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof SignupValidation>) {
-    // const newUser = await createUserAccount(values)
+    const newUser = await createUserAccount(values);
+    if (!newUser) {
+      return toast({ title: "Sign up failed. Please try again." });
+    }
   }
 
   return (
