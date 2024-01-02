f825b3a60aa236a2fe418f052df9ddfbfe5bbbf3
Mitzi17 <m_estrada17@live.com>
2024-01-01 22:12:19 -0800

    add || in line 70 so that when there is no session will be redirected to sign in page

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -67,8 +67,8 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
 
   useEffect(() => {
     if (
-      localStorage.getItem("cookieFallback") === "[]"
-      //localStorage.getItem("cookieFallback") === null
+      localStorage.getItem("cookieFallback") === "[]" ||
+      localStorage.getItem("cookieFallback") === null
     )
       navigate("/sign-in");
 
