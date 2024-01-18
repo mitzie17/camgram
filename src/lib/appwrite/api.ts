bb543194da237f6535ac217ca6dbbaf4b3afc4cf
Mitzi17 <m_estrada17@live.com>
2024-01-17 22:51:40 -0800

    define createPost to actually create post in appwrite database

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -90,3 +90,60 @@ export async function signOutAccount() {
     console.log(error);
   }
 }
+
+export async function createPost(post: INewPost) {
+  try {
+    // upload image to storage
+    const uploadedFile = await uploadFile(post.file[0]);
+    if (!uploadedFile) throw Error;
+
+    // get file url
+    const fileUrl = getFilePreview(uploadedFile.$id);
+    if (!fileUrl) {
+      deleteFile(uploadedFile.$id);
+      throw Error;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  // convert tags into an array
+}
+
+export async function uploadFile(file: File) {
+  try {
+    const uploadedFile = await storage.createFile(
+      appwriteConfig.storageId,
+      ID.unique(),
+      file
+    );
+    return uploadedFile;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getFilePreview(fileId: string) {
+  try {
+    const fileUrl = storage.getFilePreview(
+      appwriteConfig.storageId,
+      fileId,
+      2000,
+      2000,
+      "top",
+      100
+    );
+    return fileUrl;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function deleteFile(fileId: string) {
+  try {
+    await storage.deleteFile(appwriteConfig.storageId, fileId);
+    return { status: "ok" };
+  } catch (error) {
+    console.log(error);
+  }
+}
