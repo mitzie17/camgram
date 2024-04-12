19f34f181b92fc9a63ccd9474e287939f841f8cd
Mitzi17 <m_estrada17@live.com>
2024-04-11 20:17:47 -0700

    change app title

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -17,12 +17,9 @@ const Topbar = () => {
     <section className="topbar">
       <div className="flex-between py-4 px-5">
         <Link to="/" className="flex gap-3 items-center">
-          <img
-            src="/assets/images/logo.svg"
-            alt="logo"
-            width={130}
-            height={325}
-          />
+          <p className="h3-bold md:h1-semibold w-full text-primary-500">
+            Camgram
+          </p>
         </Link>
 
         <div className="flex gap-4">
